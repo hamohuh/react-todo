b8f6e4c0a8466494367936e0896e65829899821a
hamohuh <hamodroid@gmail.com>
2020-03-06 00:13:04 +0200

    Updates

---
+++ precache-manifest.d46a4405b12c46aa2c70a2fa50e2e06a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca283c41bc76568e2f33cde06ff6adae",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "a7fc3fcf50273a358498",
+    "url": "/react-todo/static/css/2.7c989d4c.chunk.css"
+  },
+  {
+    "revision": "e50f2a27e47b2d75c6da",
+    "url": "/react-todo/static/css/main.fc5637b6.chunk.css"
+  },
+  {
+    "revision": "a7fc3fcf50273a358498",
+    "url": "/react-todo/static/js/2.565ec8aa.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-todo/static/js/2.565ec8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50f2a27e47b2d75c6da",
+    "url": "/react-todo/static/js/main.6a45f267.chunk.js"
+  },
+  {
+    "revision": "168e090078e667cda6c3",
+    "url": "/react-todo/static/js/runtime-main.d35fb694.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-todo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-todo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-todo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-todo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-todo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
